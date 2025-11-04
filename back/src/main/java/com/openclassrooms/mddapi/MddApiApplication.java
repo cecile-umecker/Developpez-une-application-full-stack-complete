1700dc43d77b4b79fde757579ae2bd047c419d85
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-11-04 11:29:00 +0100

    add small starter config

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -3,10 +3,21 @@ package com.openclassrooms.mddapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class MddApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+			System.setProperty("SPRING_USER_NAME", dotenv.get("SPRING_USER_NAME"));
+			System.setProperty("SPRING_USER_PASSWORD", dotenv.get("SPRING_USER_PASSWORD"));
+			System.setProperty("DB_URL", dotenv.get("DB_URL"));
+			System.setProperty("DB_USER", dotenv.get("DB_USER"));
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+			System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+			System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));		
+		
 		SpringApplication.run(MddApiApplication.class, args);
 	}
 
