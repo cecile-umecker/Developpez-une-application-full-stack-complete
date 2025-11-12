e605ce47be7233617844f094ee096a17d34d8963
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-11-12 16:57:28 +0100

    add session persistence

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { NavbarComponent } from "./utils/navbar/navbar.component";
 import { filter } from 'rxjs';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,19 @@ export class AppComponent {
   title = 'front';
   showNavbar = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event) => {
         this.showNavbar = event.url !== '/';
       });
+  }
+
+  ngOnInit() {
+    this.authService.isAuthenticated().subscribe(isAuth => {
+      if (isAuth) {
+        this.router.navigate(['/home']);
+      }
+    });
   }
 }
