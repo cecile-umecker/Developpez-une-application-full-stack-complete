44bd4fee6f031df34a71e9c545fee89cbd15eb5e
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-11-18 14:44:43 +0100

    add material user account icom

--- front/src/app/utils/navbar/navbar.component.ts
+++ front/src/app/utils/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
