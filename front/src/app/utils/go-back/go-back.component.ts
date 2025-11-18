6984b07efe54ba683f481d33fa763178b3ca458b
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-11-18 14:22:34 +0100

    add material arrow icon

--- front/src/app/utils/go-back/go-back.component.ts
+++ front/src/app/utils/go-back/go-back.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-go-back',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './go-back.component.html',
   styleUrl: './go-back.component.scss'
 })
