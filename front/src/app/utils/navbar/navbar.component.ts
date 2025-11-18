5a16b29dd6803d2b10125533bc2d9cb79730a6bd
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-11-18 16:58:02 +0100

    add sidebar, add styling

--- front/src/app/utils/navbar/navbar.component.ts
+++ front/src/app/utils/navbar/navbar.component.ts
@@ -1,22 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenav } from '@angular/material/sidenav';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule, MatSidenavModule, MatToolbarModule, MatListModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
   isLoggedIn = false;
+  isMobile = false;
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
@@ -24,11 +33,28 @@ export class NavbarComponent {
     this.authService.authChanged$.subscribe(
       (status) => (this.isLoggedIn = status)
     );
+
+    // Detect mobile devices
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+        console.log('isMobile:', this.isMobile);
+        
+      });
   }
 
   logout(): void {
     this.authService.logout().subscribe(() => {
       this.router.navigate(['/login']);
+      if (this.isMobile && this.sidenav) {
+        this.sidenav.close();
+      }
     });
+  }
+
+  closeSidenav(): void {
+    if (this.isMobile && this.sidenav) {
+      this.sidenav.close();
+    }
   }
 }
