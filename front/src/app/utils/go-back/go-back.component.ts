8bd707e63a88d973c3ca6e5c4a65575fa8acd0da
Cécile UMECKER <74008168+cecile-umecker@users.noreply.github.com>
2025-11-19 14:47:09 +0100

    fix navigation back

--- front/src/app/utils/go-back/go-back.component.ts
+++ front/src/app/utils/go-back/go-back.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-go-back',
@@ -9,7 +11,13 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class GoBackComponent {
 
+  constructor(private router: Router, private authService: AuthService) {}
+
   goBack() {
-    window.history.back();
+    if (this.authService.isLoggedIn()) {
+      this.router.navigateByUrl('/home');
+    } else {
+      this.router.navigateByUrl('/');
+    }
   }
 }
